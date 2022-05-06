779948968c5dee35effa04459f52618d00cd5f18
Michael Kohl <me@citizen428.net>
2022-05-06 20:58:43 +0700

    Add SDL2 dependency

--- emulator/emulator.go
+++ emulator/emulator.go
@@ -1,7 +1,27 @@
 package emulator
 
-import "fmt"
+import "github.com/veandco/go-sdl2/sdl"
 
 func Run() {
-	fmt.Println("Hello, Chip-8 world!")
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("CHIP-8", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		640, 320, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+EventLoop:
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				break EventLoop
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/citizen428/chip8
 
 go 1.18
+
+require github.com/veandco/go-sdl2 v0.4.20 // indirect
