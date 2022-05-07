1223579ac9d60b930522b52561dde6cd4ef64ec7
Michael Kohl <me@citizen428.net>
2022-05-07 18:09:06 +0700

    Inline registers into chip8

--- internal/emulator/chip8.go
+++ internal/emulator/chip8.go
@@ -1,12 +1,23 @@
 package emulator
 
 const (
-	memorySize = 4096
-	stackDepth = 16
+	dataRegistersCount = 16
+	memorySize         = 4096
+	stackDepth         = 16
 )
 
 type memory [memorySize]uint8
 type stack [stackDepth]uint16
+
+// Reference: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#2.2
+type registers struct {
+	v  [dataRegistersCount]uint8 // data registers
+	i  uint16                    // generally used to store addresses
+	dt uint8                     // delay timer
+	st uint8                     // sound timer
+	pc uint16                    // program counter
+	sp uint8                     // stack pointer
+}
 
 type chip8 struct {
 	memory    memory
