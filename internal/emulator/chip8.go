05d7e98c95cfb43148b0b5b22ed69f98293c8a1e
Michael Kohl <me@citizen428.net>
2022-05-07 11:04:36 +0700

    Add stack and chip8 struct

---
+++ internal/emulator/chip8.go
@@ -0,0 +1,31 @@
+package emulator
+
+const (
+	stackDepth = 16
+)
+
+type stack [stackDepth]uint16
+
+type chip8 struct {
+	registers registers
+	stack     stack
+}
+
+func (c chip8) validateStackDepth() {
+	if c.registers.sp > stackDepth {
+		panic("Stack overflow")
+	}
+}
+
+func (c *chip8) stackPush(val uint16) {
+	c.validateStackDepth()
+	c.stack[c.registers.sp] = val
+	c.registers.sp++
+}
+
+func (c *chip8) stackPop() uint16 {
+	c.registers.sp--
+	c.validateStackDepth()
+	val := c.stack[c.registers.sp]
+	return val
+}
