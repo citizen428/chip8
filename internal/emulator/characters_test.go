8de814caaa49a7b6197a8b354233ee6bc91a9709
Michael Kohl <me@citizen428.net>
2022-05-07 16:34:00 +0700

    Add default character set

---
+++ internal/emulator/characters_test.go
@@ -0,0 +1,16 @@
+package emulator
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCharacterSetInitialization(t *testing.T) {
+	c := NewChip8()
+	want := []uint8{0xf0, 0x90, 0x90, 0x90, 0xf0}
+	got := c.memory.storage[0:5]
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
--- internal/emulator/chip8.go
+++ internal/emulator/chip8.go
@@ -7,9 +7,16 @@ const (
 type stack [stackDepth]uint16
 
 type chip8 struct {
+	memory    memory
 	registers registers
 	stack     stack
 	keyboard  keyboard
+}
+
+func NewChip8() chip8 {
+	c := chip8{}
+	copy(c.memory.storage[0:80], defaultCharacterSet)
+	return c
 }
 
 func (c chip8) validateStackDepth() {
